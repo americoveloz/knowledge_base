2a6447c751eb1e15da2f429dfb66732d08f7f0e9
Americo Veloz <americoveloz@gmail.com>
2026-08-19 16:09:19 -0400

    Add PowerShell script for Windows management tasks
    This script contains various PowerShell commands for managing Windows Updates, roles and features, HYPER-V, remote administration, and Windows Firewall settings.

---
+++ implementation/test.ps1
@@ -0,0 +1,64 @@
+# Powershell commands
+
+# Setting Windows Updates to automatic
+
+## Stop windows update service
+net stop wuauserv
+
+## Set automatic updates to 4 (enabled)
+cscript scregedit.wsf /AU 4
+
+## Start windows update service
+net start wuauserv
+
+## Downloading and installing updates
+wuauclt /detectnow
+
+#######################################################################################
+
+# Adding roles and features
+
+## List or search Windows Server roles 
+
+Get-WindowsFeature *file* # to search Fileserver role
+
+## Install Windows Server role
+
+Install-WindowsFeature -Name AD-Domain-Services, DNS -IncludeManagementTools
+
+#######################################################################################
+
+# Enable or disable HYPER-V
+bcdedit /set hypervisorlaunchtype [on|off]
+reboot
+
+#######################################################################################
+
+# Enable Remote Administration
+
+## Enabling remote administration. Enabled by default on Windows Server
+Configure-SMRemoting.exe -Enable 
+
+## Enable remote administration with PowerShell
+Enable-PSRemoting -force
+
+## Enable remote administration with winrm
+Winrm quickconfig
+
+
+#######################################################################################
+
+# Configure Windows Firewall
+
+## Enable or disable Firewall
+
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+
+## Filter Firewall rules
+Get-NetFirewallRule *icmp* | Format-table
+
+## Enable or disable firewall rule
+Set-NetFirewallRule -Name "CoreNet-Diag-ICMP4-EchoRequest-In" -Enabled False
+
+## Create new firewall rule
+New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Protocol ICMPv4 -IcmpType 8 -Direction Inbound -Action Allow
